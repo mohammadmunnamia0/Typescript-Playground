802e2a53c3f2e0f280f0506a6b09eadef945048a
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-05-05 11:44:33 +0600

    Array

---
+++ module1/dist/types.js
@@ -0,0 +1,27 @@
+"use strict";
+//string
+//implicit datatype
+let FirstName = 'Munna';
+//explicit datatype
+let LastName = 'Munna';
+let Roll = 123;
+let IsAdmin = true;
+let x = undefined;
+let y = null;
+//any typescript
+let w;
+w = 'munna';
+w = 123;
+w = true;
+w = undefined;
+w = null;
+//if i dealer a variable as a string i cant put any other value on that variable
+let m;
+// m = "string" -> not possible cause i declare m as a number
+m = 10;
+//Array
+let array = ['m', 's'];
+array.push('munna');
+console.log(array);
+//as this is a string type of array so we can only push string
+let roll = [1, 2, 3];
---
+++ module1/src/types.ts
@@ -0,0 +1,36 @@
+
+//string
+    //implicit datatype
+let FirstName = 'Munna' 
+    //explicit datatype
+let LastName : string = 'Munna'
+let Roll : number = 123
+let IsAdmin : boolean = true
+let x : undefined = undefined
+let y : null = null
+
+//any typescript
+let w;
+w = 'munna'
+w = 123
+w = true
+w = undefined
+w = null
+
+//if i dealer a variable as a string i cant put any other value on that variable
+
+let m:number;
+// m = "string" -> not possible cause i declare m as a number
+m = 10;
+
+
+
+//Array
+let array : string[]  = ['m','s']
+array.push('munna');
+console.log(array);
+//as this is a string type of array so we can only push string
+
+let roll : number[] = [1,2,3]
+
+
