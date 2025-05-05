cc20828bafd0285094d116c4257797169cfab752
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-05-05 23:07:21 +0600

    `Refactor object type and properties in module1/src/object.ts`

--- module1/src/object.ts
+++ module1/src/object.ts
@@ -1,15 +1,13 @@
-const user :
-{
-    FirstName : string; //mandatory
-    MiddleName ?: String; //Optional Type -> user jodi aita naw use kore no issue cause its optional
-    LastName : string; //mandatory
-    number : number; //mandatory
-    company : 'ReturnHex'; //Literal type cant be change in future
-
+const user: {
+  FirstName: string; //mandatory
+  MiddleName?: String; //Optional Type -> user jodi aita naw use kore no issue cause its optional
+  LastName: string; //mandatory
+  number: number; //mandatory
+  company: "ReturnHex"; //Literal type cant be change in future
+  // readonly company : 'ReturnHex'; //Literal type cant be change in future also cant be change because of using readonly
 } = {
-    FirstName:"munna",
-    LastName : 'mia',
-    number : 123,
-    company : 'ReturnHex' // i cant change the company name cause its been declare at the first as a Literal type
-}
-
+  FirstName: "munna",
+  LastName: "mia",
+  number: 123,
+  company: "ReturnHex", // i cant change the company name cause its been declare at the first as a Literal type
+};
