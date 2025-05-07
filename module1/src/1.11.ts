2bc7379dc4f54f9f60b85f03e0b773192e879193
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-05-07 15:54:28 +0600

    `Removed file: module1/src/1.11Ternary-optional-chaining-&-nullish-coalescing.ts`

---
+++ module1/src/1.11.ts
@@ -0,0 +1,60 @@
+// Ternary-optional-chaining-&-nullish-coalescing
+
+
+
+
+//Ternary
+{
+  const age: number = 15;
+
+  const isAdult = age >= 18 ? "Adult" : "Not Adult";
+  console.log(isAdult);
+  console.log({ isAdult }); // printing as a object
+}
+
+
+
+// nullish coalescing
+
+{
+  // nullish coalescing "??" -> use for null or undefine decision making, if there is a null or undefine value that time it will make a decision and set a value like guest we mention in below .
+  const isAuth = null;
+
+  const result = isAuth ?? "Guest";
+  console.log({ result });
+}
+
+
+
+
+
+//optional chaining is same as nullish coalescing make decision while there is null or Undefine value.
+
+
+{
+  type UserDetails = {
+    name: string;
+    age: number;
+    address: {
+      city: string;
+      presentAddress: string;
+      permanentAddress?: string;
+    };
+  };
+
+  const User1: UserDetails = {
+    name: "munna",
+    age: 24,
+    address: {
+      city: "Dhaka",
+      presentAddress: "Dhaka",
+    },
+  };
+
+  const CheckAddress1 = User1.address.permanentAddress;
+  console.log({CheckAddress1}); // answer is Undefine 
+
+  const CheckAddress2 = User1?.address?.permanentAddress ?? "No permanentAddress";
+  console.log({CheckAddress2}); // answer is No permanentAddress 
+
+}
