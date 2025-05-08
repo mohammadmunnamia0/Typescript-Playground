194352a8ab8e1399b60c4af9037277209bd62262
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-05-08 16:16:03 +0600

    Constraints

---
+++ module2/2.6Constraints.ts
@@ -0,0 +1,36 @@
+                                //Constraints - USe for must input validation
+{
+    
+    const addCourseToStudent = <T extends { id:number; name:string ; email:string;}>(student : T)=>{
+        // Must "id,name,email" Lagbei Lagbe
+        const Course = "NLWD";
+        return {
+            ...student,
+            Course
+        }
+    }
+
+    type studentDetails ={
+        id:number;
+        name:string ;
+        email:string;
+        age?:string;
+        laptop?:boolean;
+
+    }
+    const student1 = addCourseToStudent<studentDetails>({
+        id:1,
+        name:'munna',
+        email:'m@gmail.com'
+    })
+    const student2 = addCourseToStudent<studentDetails>({
+        id:2,
+        name:'Suraiya',
+        email:'s@gmail.com'
+    })
+    const student3 = addCourseToStudent<studentDetails>({
+        id:2,
+        name:'Suraiya',
+        email:'s@gmail.com'
+    })
+}
