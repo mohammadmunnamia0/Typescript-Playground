60d4989e5650c5885c4abcd0ef67e5acfa9a47be
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-05-17 15:31:16 +0600

    In guard

--- module3/3.3.1InGuard.ts
+++ module3/3.3.1InGuard.ts
@@ -12,7 +12,7 @@
     const getUser = (user:NormalUser|AdminUser) =>{
       /*  user. // only user.name will work here cause both type has name , not the role , role is only for the admin user. lets solve the problem using In Guard  */
 
-      if('role' in user) { // In Guard -> 'role' in user
+      if('role' in user) { //----------- In Guard -> 'role' in user
         console.log(`Hey There im  ${user.name} and my role is ${user.role}`) // this time while i write "user."" it will suggest the role 
       }
       else{
@@ -21,6 +21,14 @@
 
     }
 
+    //-----------calling the function
 
-    const user1 = getUser('Munna');
+  /*  const user1 = getUser('Munna'); //Argument of type 'string' is not assignable to parameter of type 'NormalUser | AdminUser'.ts(2345) -> Saying  je ami to argument chai nai string ami chaisi argument hobe NormalUser | AdminUser */
+
+     const user1 : NormalUser = { name: 'Munna'}; 
+     getUser(user1); //Called the function after pass the argument in 27 number line
+
+    //  const user1 = getUser({name : 'Munna'}); // pass the argument and called the function in one line
+
+     const user2 = getUser({name : 'Munna', role:'Admin'}); // pass the argument and called the function in one line
 }
