1992a520621877f89e26b03d634d1425c83b1238
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-05-19 03:03:46 +0600

    Encapsulation - > Public,private,protected

---
+++ module3/3.10Encapsulation.ts
@@ -0,0 +1,99 @@
+                                                    //Encapsulation - > Public,private,protected
+
+// * private property can only be access inside the class
+// * Private property must use the naming convention "_propertyName"
+//protected property can be access out side the parent class , but only in the child class which call is inherit the parent class
+
+{
+  // * private property can only be access inside the class
+  // * Private property must use the naming convention "_propertyName"
+
+  {
+    //-------BankAccount
+    class BankAccount {
+      constructor(
+        public readonly id: number,
+        public name: string,
+        private _balance1: number // private property only use inside the own class and  cant use or change the _balance1 out of the BankAccount class
+      ) {};
+
+      //method
+      addDeposit(amount: number) {
+        this._balance1 = this._balance1 + amount;
+      }
+
+      //method
+      getBalance() {
+        return this._balance1;
+      }
+    }
+
+    //-----Student BankAccount
+
+    class StudentAccount extends BankAccount {
+      studentDeposit(amount: number) {
+        // here if i want to access the "_balance1" property i cant access cause it is private
+        this._balance1 = this._balance1 + amount;
+      }
+    }
+
+    //------------Creating Instance and calling the methods
+
+    const UserAccount = new BankAccount(1, "munna", 1000); // UserAccount.name = we can change this , but we cant change id cause we have declare id as a readonly property
+    UserAccount.addDeposit(3000000);
+    const myBalance = UserAccount.getBalance();
+    console.log(`Your Current Balance is ${myBalance}`);
+
+    const Student = new StudentAccount(1, "Suraiya", 200);
+    Student.studentDeposit(100);
+    const StudentBalance = Student.getBalance();
+    console.log(`Your Current Balance is ${StudentBalance}`);
+  }
+}
+
+//--------------------------- Using protected ---------------------------------------------
+
+{
+    
+  {
+    //-------BankAccount
+    class BankAccount {
+      constructor(
+        public readonly id: number,
+        public name: string,
+        protected _balance1: number // can use or change the _balance1 out of the BankAccount class
+      ) {};
+
+      addDeposit(amount: number) {
+        this._balance1 = this._balance1 + amount;
+      }
+
+      getBalance() {
+        return this._balance1;
+      }
+    }
+
+    //-----Student BankAccount
+
+    class StudentAccount extends BankAccount {
+      studentDeposit(amount: number) {
+        // here if i want to access the "_balance1" property i cant access cause it is private
+        // But i can access "_balance2" cause it is protected
+        this._balance1 = this._balance1 + amount;
+      }
+    }
+
+    //------------Creating Instance and calling the methods
+
+    const UserAccount = new BankAccount(1, "munna", 1000); // UserAccount.name = we can change this , but we cant change id cause we have declare id as a readonly property
+    UserAccount.addDeposit(3000000);
+    const myBalance = UserAccount.getBalance();
+    console.log(`Your Current Balance is ${myBalance}`);
+
+    const Student = new StudentAccount(1, "Suraiya", 200);
+    Student.studentDeposit(100);
+    const StudentBalance = Student.getBalance();
+    console.log(`Your Current Balance is ${StudentBalance}`);
+  }
+
+}
--- module3/3.9Interface_Abstraction .ts
+++ module3/3.9Interface_Abstraction .ts
@@ -3,6 +3,7 @@
 
 //-------------------1.Interface
 
+//Interface class means = idea about the work 
 
 interface Vehicle { //interface class only have the method , it will never had a body .
     startEngine():void ;
@@ -18,6 +19,11 @@ class Car implements Vehicle { // to access the interface methods we have it use
     endEngine(): void {
         console.log(`Im Stopping the Engine`)
     }
+
+    test(){
+        console.log(`Hi Im testing Method - I am not the part of parent class , but still im useable without error.`)
+    }
+    //Till now what you have seen that if we didn't declare anything in the parent class, we cannot write that on the child class. But  Now, as we are using the interface, so inside the interface class, whatever the method, property, or whatever it is,in there, we can definitely have to implement or use it on the child class.And also We can use some extra variable,method. so it will not create any error.
 }
 
 //creating the class instance for calling the method
@@ -30,8 +36,41 @@ checkEngineStopping.endEngine();
 
 }
 
+//-----------------2.abstract
+
+
 {
 
-//-----------------2.abstract
+//abstract class means = idea about the work 
+abstract class Vehicle { //interface class only have the method
+
+      abstract startEngine():void;
+      abstract endEngine():void;
+}
+  
+class Car extends Vehicle { // to access the abstract methods we have it use "implements" 
+
+    startEngine(): void {
+        console.log(`Im Starting the Engine`);
+    }
+
+    endEngine(): void {
+        console.log(`Im Stopping the Engine`)
+    }
+
+    test(){
+        console.log(`Hi Im testing Method - I am not the part of parent class , but still im useable without error.`)
+    }
+   
+}
+
+//creating the class instance for calling the method
+/*
+const checkEngineStarting = new Vehicle; // Cannot create an instance of an abstract class. -> Means we cant create any instance directly from abstract class
+checkEngineStarting.startEngine();
+*/
+
+const checkEngineStarting = new Car;
+checkEngineStarting.startEngine();
 
 }
